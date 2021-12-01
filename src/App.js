b5cc1181b2c01ee93055b0bb99775a4c9b44a566
Joshua Tobler <jtobler99@knights.ucf.edu>
2021-11-30 23:22:18 -0500

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -5,16 +5,13 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
-
+// by placing the Header outside the Switch, this will load regardless of the page
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
